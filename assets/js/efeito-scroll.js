8206e4ee68b842cd0ee257e7ef7d48030f94000d
Raphael castro <raphaelcastro14@hotmail.com>
2022-11-18 21:26:11 -0300

    animação

---
+++ assets/js/efeito-scroll.js
@@ -0,0 +1,45 @@
+// Scroll animation
+
+const debouce = function (func, wait, immediate) {
+  let timeout;
+  return function (...args) {
+    const context = this;
+    const later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    const callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout - setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+};
+
+
+
+
+
+const target = document.querySelectorAll('[data-anime]');
+const animationClass = 'animate';
+
+function animeScroll() {
+  const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4);
+  target.forEach(function (elemento) {
+
+    if ((windowTop) > elemento.offsetTop) {
+      elemento.classList.add(animationClass);
+    } else {
+      elemento.classList.remove(animationClass);
+
+    }
+
+  })
+}
+animeScroll();
+
+if (target.length) {
+  window.addEventListener('scroll', debouce(function () {
+    animeScroll();
+
+  }, 200))
+};
